738ec1b597006fd0db0c56cb3d11ab95bd39b3be
Tampa <runaway_66@hotmail.de>
2018-05-27 04:27:53 +0200

    Include additional options and connection test

--- Mattermost.php
+++ Mattermost.php
@@ -32,6 +32,16 @@ class Mattermost implements NotificationModuleInterface
 				'Type' => 'text',
 				'Description' => 'Webhook URL created in Mattermost to send notifications to.',
 			],
+			'displayname' => [
+				'FriendlyName' => 'Display Name',
+				'Type' => 'text',
+				'Description' => 'Which display name to use when sending notifications',
+			],
+			'iconurl' => [
+				'FriendlyName' => 'Icon URL',
+				'Type' => 'text',
+				'Description' => 'Custom icon to use for notifications, leave empty for default',
+			],
 			'ssl' => [
 				'FriendlyName' => 'SSL',
 				'Type' => 'yesno',
@@ -41,7 +51,35 @@ class Mattermost implements NotificationModuleInterface
 	}
 	
 	public function testConnection($settings)
-    {
+    {		
+		
+		$payload = "{
+			\"username\": \"".$settings['displayname']."\",
+			\"icon_url\": \"".$settings['iconurl']."\",
+			\"text\": \"WHMCS Notification\nTEST\"}";
+		
+		$ch = curl_init();
+		
+		if ($settings['ssl'] === false)
+		{
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		}
+		curl_setopt($ch, CURLOPT_URL, $settings['webhook_url']);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		
+		$headers = array();
+		$headers[] = "Content-Type: application/json";
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		
+		$result = curl_exec($ch);
+		if (curl_errno($ch)) {
+			echo 'Error:' . curl_error($ch);
+		}
+		curl_close ($ch);
+		
 		return true;
     }
 
@@ -58,6 +96,11 @@ class Mattermost implements NotificationModuleInterface
     public function sendNotification(NotificationInterface $notification, $moduleSettings, $notificationSettings)
     {
 		
+		$payload = "{
+			\"username\": \"".$moduleSettings['displayname']."\",
+			\"icon_url\": \"".$moduleSettings['iconurl']."\",
+			\"text\": \"WHMCS Notification\n".$notification->getTitle()."\n".$notification->getMessage()."\n".$notification->getUrl()."\"}";
+		
 		$ch = curl_init();
 		
 		if ($modulesettings['ssl'] === false)
@@ -67,7 +110,7 @@ class Mattermost implements NotificationModuleInterface
 		}
 		curl_setopt($ch, CURLOPT_URL, $moduleSettings['webhook_url']);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"text\": \"WHMCS Notification\n".$notification->getTitle()."\n".$notification->getMessage()."\n".$notification->getUrl()."\"}");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		
 		$headers = array();
